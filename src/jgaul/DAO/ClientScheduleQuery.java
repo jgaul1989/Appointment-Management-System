54f52bc35ad5dc7c582dd891a064de75b439a077
jgaul1989 <jonathan.gaul1@gmail.com>
2023-02-07 11:34:26 -0700

    Third Commit

--- src/jgaul/DAO/ClientScheduleQuery.java
+++ src/jgaul/DAO/ClientScheduleQuery.java
@@ -1,26 +1,38 @@
 package jgaul.DAO;
 
+import javafx.scene.control.Label;
+import jgaul.controller.loginController;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class ClientScheduleQuery {
 
-    public static void getAllAppointments() {
+    public static boolean loginValidation(String username, String password, Label errorMessage) {
+        String validUserPassword = "";
+        boolean userNameFound = false;
 
         try {
-            String sql = "SELECT * FROM appointments";
-            PreparedStatement appointmentQuery = JDBC.getConnection().prepareStatement(sql);
-            ResultSet resultSet = appointmentQuery.executeQuery();
+            String sql = "SELECT Password FROM users WHERE User_Name = ?";
+            PreparedStatement userSearch = JDBC.getConnection().prepareStatement(sql);
+            userSearch.setString(1, username);
+            ResultSet resultSet = userSearch.executeQuery();
 
             while(resultSet.next()) {
-                int id = resultSet.getInt("Appointment_ID");
-                String title = resultSet.getString("Title");
-                System.out.println(id + " " + title);
+                validUserPassword = resultSet.getString("Password");
+                userNameFound = true;
             }
         } catch(SQLException e) {
             System.out.println(e.getMessage());
         }
-
+        if (!userNameFound) {
+            errorMessage.setText("Invalid username");
+            return false;
+        } else if (!(validUserPassword.equals(password))) {
+            errorMessage.setText("Invalid password");
+            return false;
+        }
+        return true;
     }
 }
--- src/jgaul/controller/loginController.java
+++ src/jgaul/controller/loginController.java
@@ -6,26 +6,38 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import jgaul.DAO.ClientScheduleQuery;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class loginController implements Initializable {
-    public Button button45;
+    public Button loginButton;
+    public Label usernameLabel;
+    public TextField userNameTextField;
+    public TextField passwordTextField;
+    public Label passwordLabel;
+    public Label loginErrorMessage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("I am initialized");
-
     }
 
     public void loginButtonClicked(ActionEvent actionEvent) throws IOException {
-        System.out.println("I am clicked");
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/success.fxml"));
-        Stage window = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        window.setScene(new Scene(root));
-        window.show();
+        String username = userNameTextField.getText();
+        String password = passwordTextField.getText();
+
+        if (ClientScheduleQuery.loginValidation(username, password, loginErrorMessage)) {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/success.fxml"));
+            Stage window = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+            window.setScene(new Scene(root));
+            window.show();
+        }
     }
+
 }
