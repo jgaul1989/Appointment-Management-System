ef69be19c8c034bc0e0ecb086e8fa4b1fd1e2648
jgaul1989 <jonathan.gaul1@gmail.com>
2023-02-15 16:41:10 -0700

    2/15/23 Commit

--- src/jgaul/controller/addAppointmentController.java
+++ src/jgaul/controller/addAppointmentController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import jgaul.model.Contact;
 import jgaul.model.User;
+import jgaul.model.UserAppointmentTimes;
 import jgaul.utility.Helper;
 
 import java.net.URL;
@@ -18,16 +19,18 @@ public class addAppointmentController implements Initializable {
 
     public ComboBox<Contact> contactCB;
     public ComboBox<User> userCB;
-    public ComboBox<LocalTime> startTimeCB;
-    public ObservableList<LocalTime> times = FXCollections.observableArrayList();
+    public ComboBox<UserAppointmentTimes> startTimeCB;
+    public ObservableList<UserAppointmentTimes> startTimes = FXCollections.observableArrayList();
+    public LocalTime appointmentStartTime;
     public DatePicker appointmentDateDP;
     public LocalDate selectedDate;
+    public ComboBox<UserAppointmentTimes> endTimeCB;
+    public ObservableList<UserAppointmentTimes> endTimes = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         contactCB.setItems(Helper.allContacts);
         userCB.setItems(Helper.allUsers);
-
     }
 
     public void submitAppointment(ActionEvent actionEvent) {
@@ -38,6 +41,8 @@ public class addAppointmentController implements Initializable {
 
     public void dateSelected() {
         selectedDate = appointmentDateDP.getValue();
+        startTimes.clear();
+        endTimes.clear();
         ZoneId timeZoneEST = ZoneId.of("America/New_York");
         LocalTime initial = LocalTime.of(8,0);
         LocalTime closing = LocalTime.of(22,15);
@@ -46,10 +51,22 @@ public class addAppointmentController implements Initializable {
             ZonedDateTime easternTime = ZonedDateTime.of(selectedDate, initial, timeZoneEST);
             ZonedDateTime zoneConversion = easternTime.withZoneSameInstant(ZoneId.systemDefault());
             LocalTime userTime = zoneConversion.toLocalTime();
-            times.add(userTime);
+            UserAppointmentTimes nextTime = new UserAppointmentTimes(userTime);
+            startTimes.add(nextTime);
             initial = initial.plusMinutes(15);
         }
-        startTimeCB.setItems(times);
+        startTimeCB.setItems(startTimes);
+    }
 
+    public void startTimeSelected(ActionEvent actionEvent) {
+        if (startTimeCB.getValue() == null) {
+            return;
+        }
+        appointmentStartTime = startTimeCB.getValue().getTime();
+        endTimes.clear();
+        startTimes.stream()
+                .filter(time -> time.getTime().isAfter(appointmentStartTime))
+                .forEach(time -> endTimes.add(time));
+        endTimeCB.setItems(endTimes);
     }
 }
---
+++ src/jgaul/model/UserAppointmentTimes.java
@@ -0,0 +1,23 @@
+package jgaul.model;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class UserAppointmentTimes {
+
+    private LocalTime time;
+
+    public UserAppointmentTimes(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String toString() {
+        String timePattern= "hh:mm a";
+        DateTimeFormatter formattedPattern = DateTimeFormatter.ofPattern(timePattern);
+        return formattedPattern.format(time);
+    }
+}
